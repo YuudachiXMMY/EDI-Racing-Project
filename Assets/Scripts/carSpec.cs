47d8be5d6bd324c8853468989f35c838564ee71a
YuudachiXMMY <934647325@qq.com>
2023-10-19 15:48:49 -0400

    try to recover from crash

--- Assets/Scripts/carSpec.cs
+++ Assets/Scripts/carSpec.cs
@@ -15,10 +15,7 @@ public class carSpec : MonoBehaviour
     public float automoveAngularSpeed;
     public float automoveAcceleration;
     public float automoveBaseOffset;
-<<<<<<< HEAD
     public float automoveDistanceTraveled;
-=======
->>>>>>> 08213f40738c9008731357566263d6e61cf9d9b2
 
     public string[] functionList;
 
