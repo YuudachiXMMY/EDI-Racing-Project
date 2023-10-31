c51210e0e2c5bfe2a35ae3789cd94c88012d6f82
YuudachiXMMY <934647325@qq.com>
2023-10-30 21:41:29 -0400

    finish SNOW and NIGHT events. build for 10.30_v0.2.8-demo.3

--- Assets/Prefabs/Events/ButtonEvents.cs
+++ Assets/Prefabs/Events/ButtonEvents.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 using TMPro;
 using System;
 using UnityEngine.Windows;
+using Unity.VisualScripting;
+using System.Drawing;
 
 public class ButtonEvents : MonoBehaviour
 {
+
+    public GameObject ltObj;
+    private Light lt;
+
     private float waitTime = 2.0f;
     private GameObject[] cars;
+
+    private GameObject snow;
+    private bool event6_triggered = false;
+    private bool event7_triggered = false;
+
+    private void Start()
+    {
+        lt = ltObj.GetComponent<Light>();
+        snow = GameObject.Find("SNOW");
+        snow.SetActive(false);
+    }
 
     private void Update()
     {
@@ -70,17 +87,56 @@ public class ButtonEvents : MonoBehaviour
         }
     }
 
+    public void Event_6()
+    {
+        if (!event6_triggered)
+        {
+            event6_triggered = true;
+            snow.SetActive(event6_triggered);
+
+            lt.color -= UnityEngine.Color.white * 20;
+
+            foreach (GameObject car in cars)
+            {
+                carSpec carSpecification = car.gameObject.GetComponent<carSpec>();
+                Automovement carAutomovement = car.gameObject.GetComponent<Automovement>();
+                if (System.Array.IndexOf(carSpecification.functionList, "WinterTire") < 0)
+                {
+                    carAutomovement.triggerSpeedEvent(-19, waitTime);
+                }
+            }
+        } else
+        {
+            event6_triggered = false;
+            snow.SetActive(event6_triggered);
+
+            lt.color += UnityEngine.Color.white * 20;
+        }
+    }
+
     public void Event_7()
     {
-        foreach (GameObject car in cars)
+        if (!event7_triggered)
         {
-            carSpec carSpecification = car.gameObject.GetComponent<carSpec>();
-            Automovement carAutomovement = car.gameObject.GetComponent<Automovement>();
-            if (System.Array.IndexOf(carSpecification.functionList, "FacialRecognition") != -1 ||
-                System.Array.IndexOf(carSpecification.functionList, "FingerprintDetection") != -1)
+            event7_triggered = true;
+
+            lt.color -= UnityEngine.Color.white * 100;
+
+            foreach (GameObject car in cars)
             {
-                carAutomovement.triggerSpeedEvent(20, waitTime);
+                carSpec carSpecification = car.gameObject.GetComponent<carSpec>();
+                Automovement carAutomovement = car.gameObject.GetComponent<Automovement>();
+                if (System.Array.IndexOf(carSpecification.functionList, "AssistedNightVision") != -1)
+                {
+                    carAutomovement.triggerSpeedEvent(10, waitTime);
+                }
             }
+        }
+        else
+        {
+            event7_triggered = false;
+
+            lt.color += UnityEngine.Color.white * 100;
         }
     }
 
